429f30ee64c6acfca2996a69fc2a5326dffa685f
kundup <kundup61@hotmail.com>
2024-11-30 09:49:38 -0800

    Ken image arranged and able to put onto canvas now

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
-console.log ("hello world");
+console.log("hello world");
+var kenpic = new Image();
 
+window.onload = function () {
+    // Canvas öğesini seç ve bağlamı al
+    game = document.getElementById("superfighters");
+    graph = game.getContext("2d");
 
+    // Resim kaynağını tanımla
+    kenpic.src = "images/ken1.png";
 
+    // Resim yüklendikten sonra çalıştır
+    kenpic.onload = function () {
 
+        graph.fillStyle = "yellow";
+        graph.fillRect(0, 0, game.width, game.height);
 
+        graph.strokeStyle = "black";
+        graph.moveTo(0, 0);
+        graph.lineTo(game.width, game.height);
+        graph.moveTo(game.width, 0);
+        graph.lineTo(0, game.height);
+        graph.stroke();
+
+    
+        graph.drawImage(kenpic, 0, 0);
+    };
+};
